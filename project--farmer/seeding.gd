b2f1278ff218066d88df4f0b56d842c1b48e935d
TheHighOrderOfTheGalaxyTheXVI <pmfantasy555@gmail.com>
2025-10-30 12:20:47 +0700

    rework seeding per grid

--- project--farmer/seeding.gd
+++ project--farmer/seeding.gd
@@ -1,36 +1,47 @@
 extends Node3D
 
 @onready var cast: RayCast3D = $"../SeedCast3D"
-#@onready var player_inventory = $"../../../../PlayerInventory"
+@onready var player_inventory = get_node("/root/World/PlayerInventory")
 @onready var anim: AnimationPlayer = $"../Jordax/AnimationPlayer"
-var item_active := true
+@onready var ground_gen = get_tree().get_first_node_in_group("ground_generator")
 
+var item_active := true
+var last_grid_pos: Vector2i = Vector2i(-1, -1)
 
 func plant_seed(seed_name: String) -> void:
 	cast.force_raycast_update()
 	if cast.is_colliding():
-		var hit = cast.get_collider()
-		var block = BlockGround.get_block_from_hit(hit)
-		print(hit)
-		if block and block.mode == BlockGround.Mode.TILLED and block.plant_type == PlantDatabase.PLANT_VARIANT.NONE:
-			var slot_index = Inventory.slots.find(HotBar.active_item)
-			#var item_data = player_inventory.inventory[slot_index]
-			var inv_seed_name = HotBar.active_item.item_name
+		var hit_pos = cast.get_collision_point()
+		var grid_pos = ground_gen.get_grid_pos_from_world(hit_pos)
+		var block = ground_gen.block_data[grid_pos.x][grid_pos.y]
+		print(hit_pos)
+		
+		if block.mode != BlockGround.Mode.TILLED:
+			print("Not tilled yet")
+			return
+		if block.plant_type != PlantDatabase.PLANT_VARIANT.NONE:
+			print("Already planted")
+			return
 			
-			var plant_variant = PlantDatabase.get_variant_from_seed(inv_seed_name)
-			var plant = PlantDatabase.get_plant_node(plant_variant)
-
-			if plant == null:
-				push_error("Plant is NULL, không instance được!")
-			else:
-				block.add_child(plant)
-				plant.position = Vector3(0, 0.2, 0)
-				block.plant_type = plant_variant
-				print("Planted seed name 🌴:", inv_seed_name)
-		else:
-			print("Cannot plant here!")
-	else:
-		print("Raycast miss")
+		## Active slot
+		var slot_index = Inventory.slots.find(HotBar.active_item)
+		var inv_seed_name = HotBar.active_item.item_name
+		
+		
+		var plant_variant = PlantDatabase.get_variant_from_seed(inv_seed_name)
+		var plant= PlantDatabase.get_plant_node(plant_variant)
+		
+		
+		
+		var world_pos = ground_gen.get_world_pos_from_grid(grid_pos)
+		plant.global_position = world_pos + Vector3(0, -0.04, 0)
+		
+		ground_gen.add_child(plant)
+		
+		block.plant_type = plant_variant
+		block.crop_ready = false
+		
+		print("Đã trồng 🌱:", inv_seed_name, "tại ô:", grid_pos)
 		
 
 
@@ -38,7 +49,6 @@ func is_holding_seed() -> bool:
 	#if player_inventory == null:
 		#return false
 	var slot_index = Inventory.slots.find(HotBar.active_item)
-	#var item_data = player_inventory.inventory[slot_index]
 	var item_name = HotBar.active_item.item_name
 	print("Is Holding Seed")
 	print("Active slot:", slot_index, "Item:", item_name)
