49b4230f89e4b3d13455407db0a47e7147c76a63
TheHighOrderOfTheGalaxyTheXVI <pmfantasy555@gmail.com>
2025-10-30 12:22:39 +0700

    delete unnecessary

--- project--farmer/Scripts/Resources/block_ground_data.gd
+++ project--farmer/Scripts/Resources/block_ground_data.gd
@@ -2,7 +2,5 @@ class_name BlockGroundData
 enum Mode { GRASS, CUT, TILLED }
 
 var mode: Mode = Mode.GRASS
-var has_dark_grass: bool = false
-var has_light_grass: bool = false
 var crop_ready: bool = false
 var plant_type = PlantDatabase.PLANT_VARIANT.NONE
